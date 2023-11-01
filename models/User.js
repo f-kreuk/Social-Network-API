243c57012ae61e1d33af8d4b365a8d9c91e7ac1d
Florian Kreuk <fkreuk@cartermario.com>
2023-11-01 16:36:53 -0400

    added the user schema in models; need to add virtual still

--- models/User.js
+++ models/User.js
@@ -1,1 +1,49 @@
 //User model
+
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        // utilized the match and validate properties as suggested in https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+        email: {
+            type: String,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            required: 'Email address is required',
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }, 
+        ],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
