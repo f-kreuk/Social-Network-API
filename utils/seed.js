75f951d0a214b5b0151ec6f5af8b5f5b0be3e234
Florian Kreuk <fkreuk@cartermario.com>
2023-11-02 15:23:12 -0400

    started adding seed file but commented out

---
+++ utils/seed.js
@@ -0,0 +1,21 @@
+// const connection = require('../config/connection');
+// const { User, Thought, Reaction} = require('../models');
+
+// const users = [
+//     {
+//         username: 'ab',
+//         email: 'ab@email.com'
+//     },
+//     {
+//         username: 'cd',
+//         email: 'cd@email.com'
+//     },
+//     {
+//         username: 'ef',
+//         email: 'ef@email.com'
+//     },
+//     {
+//         username: 'gh',
+//         email: 'gh@email.com'
+//     }
+// ]
