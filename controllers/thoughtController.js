40fd59d8a75934e2bea7832f0420c7a008db7756
Florian Kreuk <fkreuk@cartermario.com>
2023-11-02 17:45:15 -0400

    updated thought with export and updated thought controller with get all and post new controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,13 @@ const { User, Thought } = require("../models");
 module.exports = {
     // get all thoughts
     async getThoughts(req, res) {
-
+        try {
+            const thoughts = await Thought.find();
+            return res.status(200).json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     },
 
     //get single thought
@@ -13,7 +19,18 @@ module.exports = {
 
     // create thought
     async createThought(req, res) {
-
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findByIdAndUpdate(
+                req.body.userId,
+                {$addToSet: {thoughts: thought._id}},
+                {new: true}
+            );
+            return res.status(200).json({thought,user});
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     },
 
     // update thought
