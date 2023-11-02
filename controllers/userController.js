1d35116ccd133c74bfd889c211ec8ba7d5eceb61
Florian Kreuk <fkreuk@cartermario.com>
2023-11-02 10:55:16 -0400

    added the framework for the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,33 @@
 // this will be the controller for users
+const { User, Thought } = require("../models");
+
+module.exports = {
+
+    // get all users
+    getUsers
+
+
+    // get single user
+    getSingleUser
+
+
+    // create user
+    createUser
+
+
+    // update user
+    updateUser
+
+
+    // delete user
+    deleteUser
+
+
+    // create friend
+    createFriend
+
+
+    // delete friend
+    deleteFriend
+
+};
